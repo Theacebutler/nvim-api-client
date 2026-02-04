0be1c195ac62a3be75a7f1e10b5ecd484ce1f44a
theacebutler <theacebutler@gmail.com>
2026-02-03 22:29:56 -0500

    refactor... moved the method boxs to a enw file

--- lua/nvim-api-client/config.lua
+++ lua/nvim-api-client/config.lua
@@ -3,7 +3,13 @@ local height = math.floor(vim.o.lines * 0.8)
 local col = math.floor((vim.o.columns - width) / 2)
 local row = math.floor((vim.o.lines - height) / 2)
 local quarter = 11
+local methods = require("nvim-api-client.methods")
 local windows = {
+	methods.delete_win,
+	methods.post_win,
+	methods.put_win,
+	methods.get_win,
+	methods.patch_win,
 	background_win = {
 		win = nil,
 		buff = nil,
@@ -19,74 +25,6 @@ local windows = {
 			width = width + 2,
 			height = height + 2,
 			zindex = 1,
-		},
-	},
-	get_win = {
-		win = nil,
-		buff = nil,
-		text = "GET",
-		opts = {
-			modifiable = false,
-		},
-		config = {
-			relative = "editor",
-			border = "rounded",
-			style = "minimal",
-			row = row,
-			col = col + 1,
-			width = quarter,
-			height = 1,
-		},
-	},
-	post_win = {
-		win = nil,
-		buff = nil,
-		text = "POST",
-		opts = {
-			modifiable = false,
-		},
-		config = {
-			relative = "editor",
-			border = "rounded",
-			style = "minimal",
-			row = row,
-			col = col + quarter + 3,
-			width = quarter,
-			height = 1,
-		},
-	},
-	put_win = {
-		win = nil,
-		buff = nil,
-		text = "PUT",
-		opts = {
-			modifiable = false,
-		},
-		config = {
-			relative = "editor",
-			border = "rounded",
-			style = "minimal",
-			row = row,
-			col = col + (quarter * 2) + 5,
-			width = quarter,
-			height = 1,
-		},
-	},
-	delete_win = {
-		win = nil,
-		buff = nil,
-		text = "DELETE",
-		opts = {
-			modifiable = false,
-		},
-		config = {
-			relative = "editor",
-			border = "rounded",
-			style = "minimal",
-			row = row,
-			col = col + (quarter * 3) + 7,
-			width = quarter,
-			height = 1,
 		},
 	},
 	url = {
---
+++ lua/nvim-api-client/methods.lua
@@ -0,0 +1,93 @@
+local width = math.floor(vim.o.columns * 0.8)
+local height = math.floor(vim.o.lines * 0.8)
+local col = math.floor((vim.o.columns - width) / 2)
+local row = math.floor((vim.o.lines - height) / 2)
+local quarter = 11
+
+return {
+	get_win = {
+		win = nil,
+		buff = nil,
+		text = "GET",
+		opts = {
+			modifiable = false,
+		},
+		config = {
+			relative = "editor",
+			border = "rounded",
+			style = "minimal",
+			row = row,
+			col = col + 1,
+			width = quarter,
+			height = 1,
+		},
+	},
+	post_win = {
+		win = nil,
+		buff = nil,
+		text = "POST",
+		opts = {
+			modifiable = false,
+		},
+		config = {
+			relative = "editor",
+			border = "rounded",
+			style = "minimal",
+			row = row,
+			col = col + quarter + 3,
+			width = quarter,
+			height = 1,
+		},
+	},
+	put_win = {
+		win = nil,
+		buff = nil,
+		text = "PUT",
+		opts = {
+			modifiable = false,
+		},
+		config = {
+			relative = "editor",
+			border = "rounded",
+			style = "minimal",
+			row = row,
+			col = col + (quarter * 2) + 5,
+			width = quarter,
+			height = 1,
+		},
+	},
+	delete_win = {
+		win = nil,
+		buff = nil,
+		text = "DELETE",
+		opts = {
+			modifiable = false,
+		},
+		config = {
+			relative = "editor",
+			border = "rounded",
+			style = "minimal",
+			row = row,
+			col = col + (quarter * 3) + 7,
+			width = quarter,
+			height = 1,
+		},
+	},
+	patch_win = {
+		win = nil,
+		buff = nil,
+		text = "PATCH",
+		opts = {
+			modifiable = false,
+		},
+		config = {
+			relative = "editor",
+			border = "rounded",
+			style = "minimal",
+			row = row,
+			col = col + (quarter * 4) + 9,
+			width = quarter,
+			height = 1,
+		},
+	},
+}
