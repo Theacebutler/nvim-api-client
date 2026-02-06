5fe36e7846e9668ceae43bdaaf1239be49d6fc9e
theacebutler <theacebutler@gmail.com>
2026-02-05 20:16:53 -0500

    added border to selected window

--- lua/nvim-api-client/init.lua
+++ lua/nvim-api-client/init.lua
@@ -14,6 +14,8 @@ local function open_window(window)
 	vim.api.nvim_buf_set_lines(window.buff, 0, -1, false, { text })
 	for opt, value in pairs(window.opts) do
 		vim.api.nvim_set_option_value(opt, value, { buf = window.buff })
+		-- 0 is fully opaque, 100 is fully transparent
+		vim.api.nvim_set_option_value("winblend", 00, { win = window.win })
 	end
 	vim.keymap.set("n", "<S-TAB>", function()
 		M.down()
@@ -29,6 +31,28 @@ local function open_window(window)
 	end, { buffer = window.buff })
 	return w
 end
+local group = vim.api.nvim_create_augroup("FloatFocus", { clear = true })
+
+vim.api.nvim_create_autocmd("WinEnter", {
+	group = group,
+	callback = function()
+		-- Apply a specific highlight group to the border only in the active window
+		-- Format: "FloatBorder:NewHighlightGroup"
+		if vim.api.nvim_win_get_config(0).relative ~= "" then
+			vim.wo.winhighlight = "FloatBorder:DiagnosticOk"
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("WinLeave", {
+	group = group,
+	callback = function()
+		-- Revert to a dimmer highlight when leaving
+		if vim.api.nvim_win_get_config(0).relative ~= "" then
+			vim.wo.winhighlight = "FloatBorder:Comment"
+		end
+	end,
+})
 
 -- Toggle
 M.show = function()
@@ -46,6 +70,7 @@ M.hide = function()
 	for _, section in pairs(config) do
 		if section.win and vim.api.nvim_win_is_valid(section.win) then
 			vim.api.nvim_win_hide(section.win)
+			section.win = nil
 		end
 	end
 	M.selectable_windoes = {}
@@ -84,5 +109,9 @@ M.up = function()
 		end
 	end
 end
-
+M.set_border = function(win, border)
+	if vim.api.nvim_win_is_valid(win) then
+		vim.api.nvim_win_set_config(win, { border = border })
+	end
+end
 return M
